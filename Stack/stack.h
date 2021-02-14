e7c72a4a5b72789c15945823d98c4f66c73f7670
akon1te <anros_t@mail.ru>
2021-02-14 16:30:13 +0300

    pre-start ver

---
+++ Stack/stack.h
@@ -0,0 +1,31 @@
+#ifndef ADVANCED_CALCULATOR_STACK_H
+#define ADVANCED_CALCULATOR_STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <assert.h>
+
+#define STACK_H_INIT_CAPACITY 4
+
+typedef struct {
+    size_t elementSize;
+    int size, capacity;
+    char **data;
+} Stack;
+
+Stack *stCreate();
+
+void stPush(Stack *stack, char *data);
+
+char *stTop(Stack *stack);
+
+void stPop(Stack *stack);
+
+void stResize(Stack *stack, int newCap);
+
+void stPrint(Stack *stack);
+
+#endif //ADVANCED_CALCULATOR_STACK_H
+
