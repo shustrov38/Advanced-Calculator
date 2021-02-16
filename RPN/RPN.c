3003d29b8a8def44df0bdbd85e58495c37caea60
akon1te <anros_t@mail.ru>
2021-02-16 21:13:28 +0300

    last fixes RPN

--- RPN/RPN.c
+++ RPN/RPN.c
@@ -57,7 +57,7 @@ Stack *rpnFunc(rpnProcessor *stack, char **string, int size) {
                 stPop(stack->opStack);
             }
             stPop(stack->opStack);
-            if (IS_FUNC_1ARG(stTop(stack->opStack)) || IS_FUNC_2ARG(stTop(stack->opStack))) {
+            if (stack->opStack->size  && (IS_FUNC_1ARG(stTop(stack->opStack)) || IS_FUNC_2ARG(stTop(stack->opStack)))) {
                 stPush(stack->finalStack, stTop(stack->opStack));
                 stPop(stack->opStack);
             }
