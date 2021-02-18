ed5e41bb79d1b4048fec13244a4a9479a60a3f6a
shustrov38 <shustrov38@gmail.com>
2021-02-18 12:36:36 +0300

    ver2.7

--- Graph/graph.c
+++ Graph/graph.c
@@ -1,76 +1,72 @@
 #include "graph.h"
 
 typedef struct {
-    int *cnt, *p, *used;
-    int n; // cycle length. if exists, cycle stored in *p
-    int cycleStart, cycleEnd;
-} Data;
-
-typedef struct {
     int n; // count of unique variables
     char **variables;
-    int **g;
-    Data data;
+    int **matrix; // adjacency matrix
+    int *cnt, *p, *used;
+    int cycleLength; // after dfs cycle will be stored in *p
+    int cycleStart, cycleEnd;
 } Graph;
 
 void dfs(Graph *g, int v) {
-    if (g->data.cycleStart != -1) return; // recursive break
+    if (g->cycleStart != -1) return; // recursive break
 
-    g->data.used[v] = 1;
+    g->used[v] = 1;
 
     for (int u = 0; u < g->n; ++u) {
-        if (g->g[v][u]) {
-            if (g->data.used[u] == 0) {
-                g->data.p[u] = v;
+        if (g->matrix[v][u]) {
+            if (g->used[u] == 0) {
+                g->p[u] = v;
                 dfs(g, u);
-            } else if (g->data.used[u] == 1) {
-                g->data.cycleEnd = v;
-                g->data.cycleStart = u;
+            } else if (g->used[u] == 1) {
+                g->cycleEnd = v;
+                g->cycleStart = u;
                 return;
             }
-            g->data.cnt[v] += g->data.cnt[u] + 1;
+            g->cnt[v] += g->cnt[u] + 1;
         }
     }
 
-    g->data.used[v] = 2;
+    g->used[v] = 2;
 }
 
 void gProcess(Graph *g) {
-    g->data.used = (int *) malloc(g->n * sizeof(int));
-    assert(g->data.used != NULL && "bad mem allocation");
-    memset(g->data.used, 0, g->n * sizeof(int));
+    g->used = (int *) malloc(g->n * sizeof(int));
+    assert(g->used != NULL && "bad mem allocation");
+    memset(g->used, 0, g->n * sizeof(int));
 
-    g->data.cnt = (int *) malloc(g->n * sizeof(int));
-    assert(g->data.cnt != NULL && "bad mem allocation");
-    memset(g->data.cnt, 0, g->n * sizeof(int));
+    g->cnt = (int *) malloc(g->n * sizeof(int));
+    assert(g->cnt != NULL && "bad mem allocation");
+    memset(g->cnt, 0, g->n * sizeof(int));
 
-    g->data.p = (int *) malloc((g->n + 1) * sizeof(int));
-    assert(g->data.p != NULL && "bad mem allocation");
-    memset(g->data.p, -1, (g->n + 1) * sizeof(int));
+    g->p = (int *) malloc((g->n + 1) * sizeof(int));
+    assert(g->p != NULL && "bad mem allocation");
+    memset(g->p, -1, (g->n + 1) * sizeof(int));
 
-    g->data.n = 0;
-    g->data.cycleStart = -1;
-    g->data.cycleEnd = -1;
+    g->cycleLength = 0;
+    g->cycleStart = -1;
+    g->cycleEnd = -1;
 
     for (int v = 0; v < g->n; ++v) {
-        if (!g->data.used[v]) {
+        if (!g->used[v]) {
             dfs(g, v);
         }
     }
 
-    if (g->data.cycleStart != -1) {
+    if (g->cycleStart != -1) {
         int cycle[g->n + 1];
-        cycle[g->data.n++] = g->data.cycleStart;
-        for (int v = g->data.cycleEnd; v != -1; v = g->data.p[v]) {
-            cycle[g->data.n++] = v;
+        cycle[g->cycleLength++] = g->cycleStart;
+        for (int v = g->cycleEnd; v != -1; v = g->p[v]) {
+            cycle[g->cycleLength++] = v;
         }
-        for (int i = g->data.n - 1; i >= 0; --i) {
-            g->data.p[g->data.n - 1 - i] = cycle[i];
+        for (int i = g->cycleLength - 1; i >= 0; --i) {
+            g->p[g->cycleLength - 1 - i] = cycle[i];
         }
     }
 }
 
-static int compare(const void *a, const void *b) {
+static int compareByDependency(const void *a, const void *b) {
     int aDep = ((Expression *) a)->trueDependenciesCnt;
     int bDep = ((Expression *) b)->trueDependenciesCnt;
     return aDep - bDep;
@@ -79,116 +75,129 @@ static int compare(const void *a, const void *b) {
 void prepareVariables(Expression *e, int n) {
 #define THROW_ERROR(...) fprintf(stderr, "\n"__VA_ARGS__); exit(-1)
 
-    Graph *graph = (Graph *) malloc(sizeof(Graph));
-    assert(graph != NULL && "bad mem allocate");
+    Graph *g = (Graph *) malloc(sizeof(Graph));
+    assert(g != NULL && "bad mem allocate");
 
-    graph->n = 0;
-    graph->variables = (char **) malloc(n * sizeof(char *));
-    assert(graph->variables != NULL && "bad mem allocate");
+    g->n = 0;
+    g->variables = (char **) malloc(n * sizeof(char *));
+    assert(g->variables != NULL && "bad mem allocate");
     for (int i = 0; i < n; ++i) {
-        graph->variables[i] = (char *) malloc(10 * sizeof(char));
-        assert(graph->variables[i] != NULL && "bad mem allocate");
-        memset(graph->variables[i], 0, 10);
+        g->variables[i] = (char *) malloc(10 * sizeof(char));
+        assert(g->variables[i] != NULL && "bad mem allocate");
+        memset(g->variables[i], 0, 10);
     }
 
     for (int i = 0; i < n; ++i) {
-        if (!strlen(e[i].varName)) continue;
-        for (int j = 0; j < graph->n; ++j) {
-            if (!strcmp(graph->variables[j], e[i].varName)) {
+        if (!strlen(e[i].varName)) {
+            // expression must be calculated at the end
+            e[i].trueDependenciesCnt = n + 1;
+            continue;
+        }
+        for (int j = 0; j < g->n; ++j) {
+            if (!strcmp(g->variables[j], e[i].varName)) {
                 THROW_ERROR("There is more than one possible definition for variable '%s'", e[i].varName);
             }
         }
-        strcpy(graph->variables[graph->n++], e[i].varName);
+        strcpy(g->variables[g->n++], e[i].varName);
     }
 
 #ifdef __GRAPH_DEBUG__
     printf("variables: ");
-    for (int i = 0;i < graph->n;++i) {
-        printf("%s ", graph->variables[i]);
+    for (int i = 0;i < g->cycleLength;++i) {
+        printf("%s ", g->variables[i]);
     }
     printf("\n");
 #endif //__GRAPH_DEBUG__
 
-    graph->g = (int **) malloc(n * sizeof(int *));
-    assert(graph->g != NULL && "bad mem allocate");
+    g->matrix = (int **) malloc(n * sizeof(int *));
+    assert(g->matrix != NULL && "bad mem allocate");
     for (int i = 0; i < n; ++i) {
-        graph->g[i] = (int *) malloc(n * sizeof(int));
-        assert(graph->g[i] != NULL && "bad mem allocate");
-        memset(graph->g[i], 0, n * sizeof(int));
+        g->matrix[i] = (int *) malloc(n * sizeof(int));
+        assert(g->matrix[i] != NULL && "bad mem allocate");
+        memset(g->matrix[i], 0, n * sizeof(int));
     }
 
     for (int i = 0; i < n; ++i) {
         if (!strlen(e[i].varName)) continue;
         int A = 0, B = 0;
-        for (; A < graph->n; ++A) {
-            if (!strcmp(graph->variables[A], e[i].varName)) {
+        for (; A < g->n; ++A) {
+            if (!strcmp(g->variables[A], e[i].varName)) {
                 break;
             }
         }
         for (int j = 0; j < e[i].segCnt; ++j) {
             if (!strlen(e[i].dependencies[j])) continue;
-            for (; B < graph->n; ++B) {
-                if (!strcmp(graph->variables[B], e[i].dependencies[j])) {
+            for (; B < g->n; ++B) {
+                if (!strcmp(g->variables[B], e[i].dependencies[j])) {
                     break;
                 }
             }
 
-            if (B == graph->n) {
+            if (B == g->n) {
                 THROW_ERROR("Have an unrecognized variable '%s' in definition of '%s'", e[i].dependencies[j],
                             e[i].varName);
             }
 
-            graph->g[A][B] = 1;
+            g->matrix[A][B] = 1;
         }
     }
 
 #ifdef __GRAPH_DEBUG__
-    printGraph(graph->g, graph->n);
+    printGraph(g->matrix, g->cycleLength);
 #endif //__GRAPH_DEBUG__
 
-    gProcess(graph);
+    gProcess(g);
 
 #ifdef __GRAPH_DEBUG__
     printf("graph_cnt: ");
-    for (int i = 0; i < graph->n; ++i) {
+    for (int i = 0; i < g->cycleLength; ++i) {
         printf("%d ", res->cnt[i]);
     }
     printf("\n");
 #endif //__GRAPH_DEBUG__
 
     // has cycle
-    if (graph->data.n) {
+    if (g->cycleLength) {
         fprintf(stderr, "Have cycle in variable definition: ");
-        for (int i = 0; i < graph->data.n; ++i) {
-            fprintf(stderr, "%s", graph->variables[graph->data.p[i]]);
-            if (i + 1 != graph->data.n) {
+        for (int i = 0; i < g->cycleLength; ++i) {
+            fprintf(stderr, "%s", g->variables[g->p[i]]);
+            if (i + 1 != g->cycleLength) {
                 fprintf(stderr, " -> ");
             }
         }
         exit(-1);
     }
 
-    for (int i = 0; i < graph->n; ++i) {
+    for (int i = 0; i < g->n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (!strlen(e[j].varName)) {
-                e[j].trueDependenciesCnt = n + 1;
-            } else if (!strcmp(graph->variables[i], e[j].varName)) {
-                e[j].trueDependenciesCnt = graph->data.cnt[i];
+            if (!strlen(e[j].varName)) continue;
+            if (!strcmp(g->variables[i], e[j].varName)) {
+                e[j].trueDependenciesCnt = g->cnt[i];
                 break;
             }
         }
     }
 
-    qsort(e, n, sizeof(Expression), compare);
+    qsort(e, n, sizeof(Expression), compareByDependency);
 
 #undef THROW_ERROR
-    // TODO: there must be free of all functions
+
+    free(g->cnt);
+    free(g->p);
+    free(g->used);
+    for (int i = 0; i < g->n; ++i) {
+        free(g->matrix[i]);
+        free(g->variables[i]);
+    }
+    free(g->matrix);
+    free(g->cnt);
+    free(g);
 }
 
-void printGraph(int **g, int n) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            printf("%d ", g[i][j]);
+void printGraph(Graph *g) {
+    for (int i = 0; i < g->n; ++i) {
+        for (int j = 0; j < g->n; ++j) {
+            printf("%d ", g->matrix[i][j]);
         }
         printf("\n");
     }
